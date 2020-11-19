fab9aed55966284ff5db7958378d2f80531f8858
fadiojody <911.jodyjoseph@gmail.com>
2020-11-19 09:50:10 +0700

    pertemuan 24

--- pertemuan_20/src/components/Card.js
+++ pertemuan_20/src/components/Card.js
@@ -1,15 +1,30 @@
+// import React from 'react';
+
+// //Pertemuan 21
+// const Card = (props) => {
+//     return (
+//     <div className="card-container">
+//       <p>{props.name}</p>
+//       <p>{props.institution}</p>
+//       <p>{props.address}</p>
+//       <p>{props.phoneNumber}</p>
+//     </div>
+//     );
+//   };
+
+//   export default Card;
+
 import React from 'react';
 
-//Pertemuan 21
-const Card = (props) => {
+const Card = ({name, username, email, phone}) =>{
     return (
-    <div className="card-container">
-      <p>{props.name}</p>
-      <p>{props.institution}</p>
-      <p>{props.address}</p>
-      <p>{props.phoneNumber}</p>
-    </div>
-    );
-  };
+        <div className="card-container">
+            <p>{name}</p>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{phone}</p>
+        </div>
+    )
+}
 
-  export default Card;
+export default Card;
